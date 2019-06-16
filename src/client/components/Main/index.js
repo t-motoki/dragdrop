8014f7f90e6737230b7eaac56572f110ef9719cb
TakashiMotoki <t.motoki01@gmail.com>
2019-06-17 01:39:28 +0900

    移動(コピー)失敗時に、お知らせを表示するように実装 + 移動処理を別コンポーネントに分けた

--- src/client/components/Main/index.js
+++ src/client/components/Main/index.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 import moment from 'moment';
-import { DragDropContext } from 'react-beautiful-dnd';
+import { SnackbarProvider } from 'notistack';
 
-import CustomizedTables from '../CustomizedTables';
 import MenuBar from '../MenuBar';
-import ItemList from '../ItemList';
 import ItemChain from '../ItemChain';
+import MovingArea from '../MovingArea';
 
 import './style.scss';
 
@@ -14,44 +13,6 @@ const getItems = (count, offset = 0) =>
     id: `item-${k + offset}`,
     content: `item ${k + offset}`
   }));
-
-let itemCount = 45;
-const reorder = (list, startIndex, endIndex) => {
-  const result = Array.from(list);
-  const [removed] = result.splice(startIndex, 1);
-  result.splice(endIndex, 0, removed);
-
-  return result;
-};
-const move = (source, destination, droppableSource, droppableDestination, copy = false) => {
-  const sourceClone = Array.from(source);
-  const destClone = Array.from(destination);
-
-  console.log(sourceClone);
-  console.log(destClone);
-
-  // コピーフラグが有効の場合は移動せずにコピーする
-  // const [removed] = sourceClone.splice(droppableSource.index, 1);
-  const [removed] = copy
-    ? JSON.parse(JSON.stringify(sourceClone.slice(droppableSource.index, droppableSource.index + 1)))
-    : sourceClone.splice(droppableSource.index, 1);
-
-  if (copy) {
-    // コピーの場合はアイテムのIDを追加する
-    itemCount++;
-    removed.id = 'item-' + (itemCount - 1);
-  }
-
-  // アイテムを挿入
-  destClone.splice(droppableDestination.index, 0, removed);
-
-  // 結果を作成して返却
-  let result = {};
-  result[droppableSource.droppableId] = sourceClone;
-  result[droppableDestination.droppableId] = destClone;
-
-  return result;
-};
 
 class Main extends Component {
   constructor(props) {
@@ -67,6 +28,7 @@ class Main extends Component {
         ]
       },
       itemlist: {
+        count: 45,
         order: getItems(30),
         rjbChain: getItems(5, 30),
         pjbChain0: getItems(2, 35),
@@ -74,65 +36,18 @@ class Main extends Component {
         pjbChain2: getItems(5, 40),
       },
     };
-
-    this.createData = this.createData.bind(this);
-
   }
 
-  createData(date, time, jobchain) {
+  createData = (date, time, jobchain) => {
     return { date, time, jobchain };
   };
 
-  getList = id => this.state[this.id2List[id]];
-  onDragEnd = result => {
-    const { source, destination } = result;
-
-    // 移動範囲外の場合はそのまま返す
-    if (!destination) {
-      return;
-    }
-
-    let state = JSON.parse(JSON.stringify(this.state.itemlist));
-    if (source.droppableId === destination.droppableId) {
-      // 同じ場所へ移動する場合の操作
-      state[source.droppableId] = reorder(
-        state[source.droppableId],
-        source.index,
-        destination.index
-      );
-    } else {
-
-      // 移動(コピー)してはいけない場所はそのまま返す
-      if (destination.droppableId === 'order' || destination.droppableId === 'rjbChain') {
-        return;
-      }
-
-      // 別の場所へ移動(コピー)できない
-      if(source.droppableId === 'rjbChain'){
-        return;
-      }
-
-      // 別の場所へ移動(コピー)する場合の操作
-      let result = move(
-        state[source.droppableId],
-        state[destination.droppableId],
-        source,
-        destination,
-        source.droppableId === 'order',
-      );
-
-      // 変更後の値をセット
-      state[source.droppableId] = result[source.droppableId];
-      state[destination.droppableId] = result[destination.droppableId];
-
-    }
-
+  // 子コンポーネントに更新してもらう
+  onDragEnd = state => {
     this.setState({
       itemlist: state
     });
-
-  };
-
+  }
 
   render() {
 
@@ -163,18 +78,9 @@ class Main extends Component {
     return (
       <div className='main'>
         <MenuBar />
-        <DragDropContext onDragEnd={this.onDragEnd}>
-          <div className='joblist'>
-            <ItemList droppableId={'order'} items={this.state.itemlist.order} />
-          </div>
-          <div className='chainList'>
-            <div>■実際のチェーン</div>
-            <CustomizedTables head={{ bgcolor: 'gold' }} rows={rows1} idpre='ano' />
-            <br /><br />
-            <div>■時間帯別予測のチェーン</div>
-            <CustomizedTables head={{ bgcolor: 'palegreen' }} rows={rows2} idpre='yos' />
-          </div>
-        </DragDropContext>
+        <SnackbarProvider maxSnack={1} anchorOrigin={{vertical: 'top', horizontal: 'right',}}>
+          <MovingArea rows1={rows1} rows2={rows2} itemlist={this.state.itemlist} onDragEnd={this.onDragEnd}/>
+        </SnackbarProvider>
       </div>
     );
   }
---
+++ src/client/components/MovingArea/index.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import { DragDropContext } from 'react-beautiful-dnd';
+import { useSnackbar } from 'notistack';
+
+import ItemList from '../ItemList';
+import CustomizedTables from '../CustomizedTables';
+
+export default (Props) => {
+  const { enqueueSnackbar } = useSnackbar();
+
+  const reorder = (list, startIndex, endIndex) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+
+    return result;
+  };
+
+  const move = (source, destination, droppableSource, droppableDestination, copy = false) => {
+    const sourceClone = Array.from(source);
+    const destClone = Array.from(destination);
+
+    // コピーフラグが有効の場合は移動せずにコピーする
+    // const [removed] = sourceClone.splice(droppableSource.index, 1);
+    const [removed] = copy
+      ? JSON.parse(JSON.stringify(sourceClone.slice(droppableSource.index, droppableSource.index + 1)))
+      : sourceClone.splice(droppableSource.index, 1);
+
+    if (copy) {
+      // コピーの場合はアイテムのIDを追加する
+      removed.id = 'item-' + Props.itemlist.count;
+      Props.itemlist.count++;
+    }
+
+    // アイテムを挿入
+    destClone.splice(droppableDestination.index, 0, removed);
+
+    // 結果を作成して返却
+    let result = {};
+    result[droppableSource.droppableId] = sourceClone;
+    result[droppableDestination.droppableId] = destClone;
+
+    return result;
+  };
+
+  const onDragEnd = result => {
+    const { source, destination } = result;
+
+    // 移動範囲外の場合はそのまま返す
+    if (!destination) {
+      return;
+    }
+
+    let state = JSON.parse(JSON.stringify(Props.itemlist));
+    if (source.droppableId === destination.droppableId) {
+      // 同じ場所へ移動する場合の操作
+      state[source.droppableId] = reorder(
+        state[source.droppableId],
+        source.index,
+        destination.index
+      );
+    } else {
+
+      // 移動(コピー)してはいけない場所はそのまま返す
+      if (destination.droppableId === 'order' || destination.droppableId === 'rjbChain') {
+        enqueueSnackbar('ここに移動はできません。', { variant: 'error' });
+        return;
+      }
+
+      // 別の場所へ移動(コピー)できない
+      if (source.droppableId === 'rjbChain') {
+        enqueueSnackbar('ここからは、別の場所へ移動することができません。', { variant: 'error' });
+        return;
+      }
+
+      // 別の場所へ移動(コピー)する場合の操作
+      let result = move(
+        state[source.droppableId],
+        state[destination.droppableId],
+        source,
+        destination,
+        source.droppableId === 'order',
+      );
+      // 変更後の値をセット
+      state[source.droppableId] = result[source.droppableId];
+      state[destination.droppableId] = result[destination.droppableId];
+    }
+    state.count = Props.itemlist.count;
+    Props.onDragEnd(state);
+  };
+
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div className='joblist'>
+        <ItemList droppableId={'order'} items={Props.itemlist.order} />
+      </div>
+      <div className='chainList'>
+        <div>■実際のチェーン</div>
+        <CustomizedTables head={{ bgcolor: 'gold' }} rows={Props.rows1} idpre='ano' />
+        <br /><br />
+        <div>■時間帯別予測のチェーン</div>
+        <CustomizedTables head={{ bgcolor: 'palegreen' }} rows={Props.rows2} idpre='yos' />
+      </div>
+    </DragDropContext>
+  );
+}
